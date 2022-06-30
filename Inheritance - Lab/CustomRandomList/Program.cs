ee9cff1c14020898863a2a3d0427c15c8a4e28c8
Georgiiw <georgiivanovk@abv.bg>
2022-06-30 13:36:24 +0300

    done

---
+++ Inheritance - Lab/CustomRandomList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            RandomList list = new RandomList();
+
+            list.Add("nameOne");
+            list.Add("nameTwo");
+            list.Add("nameThree");
+            list.Add("nameSecret");
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
