886bba3a922cad25d4ea6ebddbc35cc49c49f8c5
Georgiiw <georgiivanovk@abv.bg>
2022-06-30 13:57:39 +0300

    done

---
+++ Inheritance - Lab/CustomStack/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(new List<string>() { "1", "2", "3" });
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
