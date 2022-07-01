a23b14515ec79a5bdd3f9a68e35ff1f5086a6e71
Georgiiw <georgiivanovk@abv.bg>
2022-07-01 12:41:26 +0300

    done

---
+++ Inheritance - Exercise/PlayersAndMonsters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int level = int.Parse(Console.ReadLine());
+            SoulMaster soulmaster = new SoulMaster(name, level);
+            Console.WriteLine(soulmaster);
+        }
+    }
+}
