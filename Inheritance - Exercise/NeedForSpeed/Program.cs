972bb6533586e4810fc4b4c391d207cc92b71fb4
Georgiiw <georgiivanovk@abv.bg>
2022-07-04 18:11:09 +0300

    done

---
+++ Inheritance - Exercise/NeedForSpeed/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int horsePower = int.Parse(Console.ReadLine());
+            double fuel = double.Parse(Console.ReadLine());
+            double kilometres = double.Parse(Console.ReadLine());
+            SportCar sportCar = new SportCar(horsePower, fuel);
+            sportCar.Drive(kilometres);
+            Console.WriteLine($"Kilometres driven: {kilometres}, Fuel consumed: {sportCar.Fuel}");
+
+        }
+    }
+}
---
+++ Inheritance - Exercise/NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        public RaceMotorcycle(int horsePower, double fuel)
+         : base(horsePower, fuel)
+        {
+
+        }
+        public override void Drive(double kilometers)
+        {
+            this.FuelConsumption = 8;
+            this.Fuel -= kilometers * this.FuelConsumption;
+
+        }
+    }
+}
