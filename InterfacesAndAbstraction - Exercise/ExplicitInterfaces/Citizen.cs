811888359769b743ef37fc298d6ce424927245c1
Georgiiw <georgiivanovk@abv.bg>
2022-07-12 15:09:50 +0300

    100

---
+++ InterfacesAndAbstraction - Exercise/ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+        public Citizen(string name, string country, int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+        void IPerson.GetName()
+        {
+            Console.WriteLine(this.Name);
+        }
+        void IResident.GetName()
+        {
+            Console.WriteLine($"Mr/Ms/Mrs {this.Name}");
+        }
+    }
+}
---
+++ InterfacesAndAbstraction - Exercise/ExplicitInterfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExplicitInterfaces
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<Citizen> citizen = new List<Citizen>();
+            while (true)
+            {
+                string[] cmd = Console.ReadLine().Split(" ");
+                if (cmd[0] == "End")
+                {
+                    break;
+                }
+                string name = cmd[0];
+                string country = cmd[1];
+                int age = int.Parse(cmd[2]);
+                Citizen newCitizen = new Citizen(name, country, age);
+                citizen.Add(newCitizen);
+            }
+            foreach (var human in citizen)
+            {
+                IPerson person = human;
+                person.GetName();
+                IResident resident = human;
+                resident.GetName();
+            }
+        }
+    }
+}
